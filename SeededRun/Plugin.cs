09aad93dafb37b2fcc21a0f53fb77774a9c569a5
PianoPrinter <pianodevelop@gmail.com>
2025-02-17 12:06:20 +0900

    Initial commit

---
+++ SeededRun/Plugin.cs
@@ -0,0 +1,66 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SeededRun;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("White Knuckle.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+    private static string seed = "";
+
+    private static bool IsSeeded() => bool.Parse(SettingsManager.GetSetting("g_seeded"));
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+
+        var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        {
+            var origin = AccessTools.Method(typeof(WorldLoader), "Awake");
+            var patch = AccessTools.Method(typeof(MyPatches), "Awake_MyPatch");
+            harmony.Patch(origin, new HarmonyMethod(patch));
+        }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnGUI()
+    {
+        if (SceneManager.GetActiveScene().name.Equals("Main-Menu") && IsSeeded())
+            seed = GUI.TextField(new Rect(10, 10, 200, 20), seed, 25);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name.Equals("Main-Menu")) AddSeededRunToggle();
+    }
+
+    private void AddSeededRunToggle()
+    {
+        var gMan = GameObject.Find("GameManager");
+        var mutators = gMan.transform.Find("Canvas/Play Menu/Play Pane/Mutators");
+        var compToggle = mutators.Find("Competitive Toggle");
+
+        var seededToggle = Instantiate(compToggle.gameObject, mutators);
+        seededToggle.transform.SetSiblingIndex(1);
+        seededToggle.name = "Seeded Run Toggle";
+        seededToggle.GetComponent<ToggleSettingsBinder>().settingName = "g_seeded";
+
+        var seededToggleLabel = seededToggle.transform.Find("Background/Label (1)");
+        seededToggleLabel.GetComponent<TextMeshProUGUI>().SetText("Seeded Run\n(Top Left Input)");
+    }
+
+    public class MyPatches
+    {
+        public static void Awake_MyPatch()
+        {
+            if (IsSeeded()) Random.InitState(int.Parse(seed));
+        }
+    }
+}
---
+++ SeededRunPatcher/Patcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Mono.Cecil;
+
+public static class Patcher
+{
+    public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
+
+    public static void Patch(AssemblyDefinition assembly)
+    {
+        var gsClass = assembly.MainModule.GetType("SettingsManager/GameSettings");
+        gsClass.Fields.Add(new FieldDefinition("g_seeded", FieldAttributes.Public, assembly.MainModule.TypeSystem.Boolean));
+    }
+}
